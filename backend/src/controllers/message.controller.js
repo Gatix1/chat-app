5160082b1668c10f4453f3a3469dc808eee1be4b
Maxim Pintea <maxim.pintea7@gmail.com>
2025-04-23 23:41:53 +0200

    implemented message model and controllers for it

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,64 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error." });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: otherId } = req.params; // otherId is the id of the user who you are chatting with
+        const id = req.user._id; // my id
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: id, receiverId: otherId},
+                {senderId: otherId, receiverId: id} 
+            ]
+        })
+        return res.status(200).json(messages);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error." });
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { id: otherId } = req.params; // otherId is the id of the user who you are chatting with
+        const id = req.user._id; // my id
+        const { text, image } = req.body;
+        if (!image && !text) {
+            return res.status(400).json({ message: "Text or image is required." });
+        }
+        let imageUrl = undefined;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId: id,
+            receiverId: otherId,
+            text,
+            image: imageUrl
+        });
+
+        const message = await newMessage.save();
+
+        //TODO: Realtime functionality goes here
+
+        return res.status(201).json(message);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error." });
+    }
+}
