03938ca6c2f15fce63bd889177cffdffda0e88a3
Maxim Pintea <maxim.pintea7@gmail.com>
2025-04-24 18:38:42 +0200

    finished navbar component

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -36,5 +36,16 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isSigningUp: false });
         }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            toast.success("Logged out successfully!");
+            set({ authUser: null });
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        }
     }
 }));
